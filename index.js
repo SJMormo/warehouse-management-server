e1da890c4e3456991ce0fe8a8a4d6d0240390326
SJMormo <sjm91097@gmail.com>
2022-05-06 13:53:39 +0600

    data are sent to client loaded from server

--- index.js
+++ index.js
@@ -13,12 +13,27 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u92dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('db connected');
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const inventoryCollection = client.db('warehouse-management').collection('inventory');
+
+        // load inventory data to frontend
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const inventory = await cursor.toArray();
+            res.send(inventory);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
